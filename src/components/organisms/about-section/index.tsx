419498afa139affdebf050b4b2a4d1d6d5efef9a
Jahid-11013 <jahid-11013@Jahid-11013s-MacBook-Pro.local>
2021-08-04 15:42:43 +0600

    some changes in heor section

---
+++ src/components/organisms/about-section/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <section className="o-about">fg</section>;
+};
+
+export default About;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroSection from "components/organisms/hero-section";
 import BlackHeroSection from "components/molecules/black-hero-section";
+import About from "components/organisms/about-section";
 
 const Home = () => {
   return (
     <div className="p-home">
       <HeroSection />
+      <About />
       {/* <BlackHeroSection /> */}
     </div>
   );
