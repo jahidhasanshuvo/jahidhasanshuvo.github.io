fb533d98cc1965c587dddc0790a9d850fc6bd649
Jahid-11013 <jahid-11013@Jahid-11013s-MacBook-Pro.local>
2021-09-06 12:03:45 +0600

    footer section design

---
+++ src/components/organisms/footer/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="o-footer">
+      <span>Copyright Md. Jahid Hasan Shuvo</span>
+      <span>
+        Check code{" "}
+        <a href="#" target="_blank">
+          here
+        </a>
+      </span>
+    </section>
+  );
+};
+
+export default Footer;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,7 @@ import About from "components/organisms/about";
 import Experience from "components/organisms/experience";
 import Works from "components/organisms/works";
 import Contact from "components/organisms/contact";
+import Footer from "components/organisms/footer";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <Experience />
       <Works />
       <Contact />
+      <Footer />
     </div>
   );
 };
